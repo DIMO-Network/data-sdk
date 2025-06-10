a7bb6ee69a7b89a70fffdc695e80f6941c63dfee
Yusuf Çırak <yusuf10901@hotmail.com>
2025-06-10 22:17:07 +0300

    feat: add vehicle events api

--- src/api/resources/DimoRestResources.ts
+++ src/api/resources/DimoRestResources.ts
@@ -1,9 +1,21 @@
-import { Attestation } from './Attestation';
-import { Auth } from './Auth';
-import { DeviceDefinitions } from './DeviceDefinitions';
-import { Devices } from './Devices';
-import { TokenExchange } from './TokenExchange';
-import { Trips } from './Trips';
-import { Valuations } from './Valuations';
+/** @format */
 
-export { Attestation, Auth, DeviceDefinitions, Devices, TokenExchange, Trips, Valuations };
+import { VehicleEvents } from "./VehicleEvents";
+import { Attestation } from "./Attestation";
+import { Auth } from "./Auth";
+import { DeviceDefinitions } from "./DeviceDefinitions";
+import { Devices } from "./Devices";
+import { TokenExchange } from "./TokenExchange";
+import { Trips } from "./Trips";
+import { Valuations } from "./Valuations";
+
+export {
+	Attestation,
+	Auth,
+	DeviceDefinitions,
+	Devices,
+	TokenExchange,
+	Trips,
+	Valuations,
+	VehicleEvents,
+};
