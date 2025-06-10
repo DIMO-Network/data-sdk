275145a6d423d3b48ab3d15d964127747c32f4b2
Yusuf Çırak <yusuf10901@hotmail.com>
2025-06-10 22:18:28 +0300

    chore: jest.config update for being able to import with api paths

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,19 @@
+/** @format */
+
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',  // Transform TypeScript files
-    '^.+\\.mjs$': 'babel-jest', // Transform .mjs files using Babel
-    '^.+\\.js$': 'babel-jest',  // Transform JavaScript files
-  },
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'mjs'],  
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'  // Adjust this if you're using path aliases
-  }
+	preset: "ts-jest",
+	testEnvironment: "node",
+	roots: ["<rootDir>/src"],
+	testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+	transform: {
+		"^.+\\.tsx?$": "ts-jest", // Transform TypeScript files
+		"^.+\\.mjs$": "babel-jest", // Transform .mjs files using Babel
+		"^.+\\.js$": "babel-jest", // Transform JavaScript files
+	},
+	transformIgnorePatterns: ["<rootDir>/node_modules/"],
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node", "mjs"],
+	moduleNameMapper: {
+		"^api/(.*)$": "<rootDir>/src/api/$1",
+		"^@/(.*)$": "<rootDir>/src/$1",
+	},
 };
