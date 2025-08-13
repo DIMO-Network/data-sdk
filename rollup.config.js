57ddfa7c62b3f94cf54ec60926a4cb197f366282
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2025-08-13 03:00:33 +0200

    [chore] add externals

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,14 @@ import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import globals from 'rollup-plugin-node-globals';
 
+const external = [
+  // Node built-ins:
+  'node:util','util','node:stream','stream','path','fs','events','buffer','os','http','https','url','zlib','tty'
+];
+
 export default {
   input: './src/index.ts',
+  external: external,
   output: [
     {
       dir: './dist',
@@ -19,7 +25,7 @@ export default {
       format: 'cjs',
       sourcemap: true,
       plugins:[
-        globals(),        
+        globals(), // Provides global variables for Node.js built-ins
       ]
     },
   ],
@@ -34,7 +40,7 @@ export default {
       extensions: ['.js', '.ts'],
     }), // Resolves node_modules and relative paths     
     commonjs(),// Converts CommonJS modules to ES6   
-    json(),    
+    json(), // Allows importing JSON files
   ],
   onwarn(warning, warn) {
     // Print detailed warnings
